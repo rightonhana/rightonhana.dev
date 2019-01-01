9465df4fc5678585db79a681c8acfbd456515df0
Maria Florencia Gonzalez <rightonhana@gmail.com>
2019-01-01 13:56:04 -0300

    New build

---
+++ build/precache-manifest.662ff15727e041491b9702a796bc3217.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b888e22afac53d97636b",
+    "url": "/static/css/main.65d5b09d.chunk.css"
+  },
+  {
+    "revision": "b888e22afac53d97636b",
+    "url": "/static/js/main.b888e22a.chunk.js"
+  },
+  {
+    "revision": "4c5d6c6a7f13c2b9ef2f",
+    "url": "/static/js/1.4c5d6c6a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "be6d84338ed41ab6e001cb93517639d1",
+    "url": "/index.html"
+  }
+];
