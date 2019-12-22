fd057fb5281c3157ac1bcfcfab39da93367288d9
Maria Florencia Gonzalez <rightonhana@gmail.com>
2019-12-22 16:09:54 -0300

    Removed some comments

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,11 @@ import IconLinks from "./components/IconLinks";
 import Logo from "./components/Logo";
 import links from "./links";
 
-export const App = () => {
-  /*const [state, setState] = useState(0);
-
-  const calculateXY = ({ clientX: number, clientY: number }) => {
-  }
-
-  const reset = () => {
-  }
-  
-  onMouseMove={calculateXY} onMouseLeave={reset}
-  */
-
-  return (
-    <>
-      <Logo color="var(--logo)" size={18} />
-      <IconLinks links={links} />
-    </>
-  );
-}
+export const App = () => (
+  <>
+    <Logo color="var(--logo)" size={18} />
+    <IconLinks links={links} />
+  </>
+);
 
 export default App;
