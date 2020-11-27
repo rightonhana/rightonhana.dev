f9526f9b8080d3b963054197120d112cf29d4109
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-11-26 22:33:41 -0800

    :wrench: now is a pwa!

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
+import { generateSW } from 'rollup-plugin-workbox';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -48,7 +49,12 @@ export default {
 			},
 			preprocess: sveltePreprocess(),
 		}),
-
+		generateSW(
+			{
+				swDest: './public/sw.js',
+				globDirectory: './public'
+			}
+		),
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
 		// some cases you'll need additional configuration -
