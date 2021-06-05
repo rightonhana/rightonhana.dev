2e52580b1e47e0b06fe812252e30dfafb51312e4
Florencia Gonzalez <rightonhana@gmail.com>
2021-06-05 13:16:59 -0700

    :hammer: refactor Hexagon component

--- src/components/Hexagon.svelte
+++ src/components/Hexagon.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
-	export let size = 10;
 </script>
 
-<div style="--size: {size}vmin;">
-        <slot />
+<div>
+	<slot />
 </div>
 
 <style>
 	div {
-                background-color: var(--hexagon);
-                background-position: 50%;
-                background-repeat: no-repeat;
-                border-radius: 50%;
-                clip-path: polygon(6.7% 25%, 50% 0%, 93.3% 25%, 93.3% 75%, 50% 100%, 6.7% 75%);
-                display: inline-block;
-                height: var(--size);
-                left: 50%;
-                top: 50%;
-                width: var(--size);
+		background-color: var(--hexagon);
+		background-position: 50%;
+		background-repeat: no-repeat;
+		border-radius: 50%;
+		clip-path: polygon(6.7% 25%, 50% 0%, 93.3% 25%, 93.3% 75%, 50% 100%, 6.7% 75%);
+		display: inline-block;
+		height: var(--size, 10vmin);
+		left: 50%;
+		top: 50%;
+		width: var(--size, 10vmin);
 	}
 </style>
