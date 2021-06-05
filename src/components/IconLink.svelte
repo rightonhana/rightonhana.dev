568668f50b910dce8555b71aab846e2dac8fe545
Florencia Gonzalez <rightonhana@gmail.com>
2021-06-05 13:21:16 -0700

    :hammer: refactor IconLink component

--- src/components/IconLink.svelte
+++ src/components/IconLink.svelte
@@ -1,31 +1,24 @@
 <script lang="ts">
-    import Hexagon from "./Hexagon.svelte";
-    import ReactiveLink from "./ReactiveLink.svelte";
-    import type LinkProps from "../types/LinkProps";
-    import type HexagonProps from "../types/HexagonProps";
-    import MediaIcon from "./MediaIcon.svelte";
+	import Hexagon from "./Hexagon.svelte";
+	import ReactiveLink from "./ReactiveLink.svelte";
+	import type LinkProps from "../types/LinkProps";
+	import MediaIcon from "./MediaIcon.svelte";
 
-    export let angle: number;
-    export let color: string;
-    export let href: string;
-    export let size: number;
-    export let title: string;
-    export let path: string;
+	export let angle: number;
+	export let color: string;
+	export let href: string;
+	export let size: number;
+	export let title: string;
+	export let path: string;
 
-    const linkProps: LinkProps = {
-        angle: -angle,
-        color,
-        href,
-        title
-    };
-
-    const hexagonProps: HexagonProps = {
-        size
-    };
+	const linkProps: LinkProps = {
+		href,
+		title
+	};
 </script>
 
-<ReactiveLink {...linkProps}>
-    <Hexagon {...hexagonProps}>
-        <MediaIcon path={path}/>
-    </Hexagon>
+<ReactiveLink {...linkProps} --angle={`-${angle}deg`} --color={color}>
+	<Hexagon --size={`${size}vmin`}>
+		<MediaIcon path={path}/>
+	</Hexagon>
 </ReactiveLink>
