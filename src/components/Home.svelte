319692edf493a4b2d42b1ffeaa9143bd43ffd7c3
Florencia Gonzalez <rightonhana@gmail.com>
2021-06-05 13:23:56 -0700

    :hammer: update Home component

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-        import IconLinks from "./IconLinks.svelte";
-        import Logo from "./Logo.svelte";
-        import links from "../constants/links"
+	import IconLinks from "./IconLinks.svelte";
+	import Logo from "./Logo.svelte";
+	import links from "../constants/links"
 </script>
 
 
-<Logo color="var(--logo)" size={18} />
+<Logo --size="18vmin" />
 <IconLinks links={links} />
