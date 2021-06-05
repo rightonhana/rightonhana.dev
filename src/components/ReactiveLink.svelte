3e970925e3327fc3dea08dd59d24fbfb28221e6f
Florencia Gonzalez <rightonhana@gmail.com>
2021-06-05 13:18:34 -0700

    :hammer: refactor ReactiveLink component

--- src/components/ReactiveLink.svelte
+++ src/components/ReactiveLink.svelte
@@ -1,38 +1,35 @@
 <script lang="ts">
-    export let angle = 0;
-    export let color = "#000";
-    export let href: string;
-    export let title = "";
+	export let href: string;
+	export let title = "";
 </script>
 
 <a rel="noopener noreferrer"
-    style="--angle:{angle}deg; --color: {color};"
-    href="{href}"
-    title="{title}"
+	href="{href}"
+	title="{title}"
 >
-    <slot />
+	<slot />
 </a>
 
 <style>
 	a {
-        border-radius: 50%;
-        color: var(--icon);
-        display: inline-block;
-        overflow: hidden;
-        pointer-events: all;
-        transform: rotate(var(--angle)) scale(1);
-        will-change: opacity, transform;
-        transition: opacity 0.2s ease-in-out, transform 0.2s ease-in-out;
+		border-radius: 50%;
+		color: var(--icon);
+		display: inline-block;
+		overflow: hidden;
+		pointer-events: all;
+		transform: rotate(var(--angle, 0)) scale(1);
+		will-change: opacity, transform;
+		transition: opacity 0.2s ease-in-out, transform 0.2s ease-in-out;
 	}
 
-    a:hover,
-    a:focus {
-        outline: 0;
-        transform: rotate(var(--angle)) scale(1.1);
-    }
+	a:hover,
+	a:focus {
+		outline: 0;
+		transform: rotate(var(--angle, 0)) scale(1.1);
+	}
 
-    a:hover,
-    a:focus {
-        color: var(--color);
-    }
+	a:hover,
+	a:focus {
+		color: var(--color, #000);
+	}
 </style>
