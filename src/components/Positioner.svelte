3c5cbde18b30d3a9974dd953cbf4cfa7a4254e94
Florencia Gonzalez <rightonhana@gmail.com>
2021-06-05 13:20:03 -0700

    :hammer: refactor Positioner component

--- src/components/Positioner.svelte
+++ src/components/Positioner.svelte
@@ -1,25 +1,23 @@
 <script lang="ts">
-    export let angle = 0;
-    export let distance = 0;
 </script>
 
-<div style="--angle: {angle}deg; --distance: {distance}vmin;">
-    <slot/>
+<div>
+	<slot/>
 </div>
 
 <style>
 	div {
-        left: 50%;
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        pointer-events: none;
-        position: absolute;
-        text-align: right;
-        top: 50%;
-        transform-origin: 0 50%;
-        transform: translate(0, -50%) rotate(var(--angle));
-        user-select: none;
-        width: var(--distance);
+		left: 50%;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		pointer-events: none;
+		position: absolute;
+		text-align: right;
+		top: 50%;
+		transform-origin: 0 50%;
+		transform: translate(0, -50%) rotate(var(--angle, 0));
+		user-select: none;
+		width: var(--distance, 0);
 	}
 </style>
