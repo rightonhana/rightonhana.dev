b31a9c64bc1df858dd4404851042895924f7f6ba
Florencia Gonzalez <rightonhana@gmail.com>
2021-06-05 13:22:30 -0700

    :hammer: refactor IconLinks component

--- src/components/IconLinks.svelte
+++ src/components/IconLinks.svelte
@@ -1,27 +1,25 @@
 <script lang="ts">
-  import type Link from "../types/Link";
-  import type LinkToRender from "../types/LinkToRender";
-  import { setAngle,setDistance } from "../utils/angles";
-  import IconLink from "./IconLink.svelte";
-  import Positioner from "./Positioner.svelte";
+	import type Link from "../types/Link";
+	import type LinkToRender from "../types/LinkToRender";
+	import { setAngle,setDistance } from "../utils/angles";
+	import IconLink from "./IconLink.svelte";
+	import Positioner from "./Positioner.svelte";
 
+	export let links: Link[] = [];
 
-
-  export let links: Link[] = [];
-
-  const linksToRender: LinkToRender[] = links
-    .map((link, index) => ({
-      ...link,
-      angle: setAngle(index),
-      distance: setDistance(index),
-    }))
-    .sort((linkA, linkB) => (linkA.tab > linkB.tab ? 1 : -1));
+	const linksToRender: LinkToRender[] = links
+		.map((link, index) => ({
+			...link,
+			angle: setAngle(index),
+			distance: setDistance(index),
+		}))
+		.sort((linkA, linkB) => (linkA.tab > linkB.tab ? 1 : -1));
 </script>
 
 <nav>
-  {#each linksToRender as { angle, color, distance, href, path, title }}
-    <Positioner {angle} {distance}>
-      <IconLink {angle} {color} {href} {path} {title} size={18} />
-    </Positioner>
-  {/each}
+	{#each linksToRender as { angle, color, distance, href, path, title }}
+		<Positioner --angle={`${angle}deg`} --distance={`${distance}vmin`}>
+			<IconLink {angle} {color} {href} {path} {title} size={18} />
+		</Positioner>
+	{/each}
 </nav>
