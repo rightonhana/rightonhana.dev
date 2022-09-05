5eb630f5137e0690ccd32c9d3a635715f702bfb7
Florencia Gonzalez <rightonhana@gmail.com>
2022-09-04 18:53:34 -0700

    :sparkles: add vercel edge functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte(), vercel()],
 });
