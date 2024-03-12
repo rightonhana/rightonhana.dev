7f48f514f6fbe0cc0abaa54a0f79f4279517fa1c
Florencia Gonzalez <rightonhana@gmail.com>
2024-03-12 14:48:55 -0500

    :arrow_up: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
