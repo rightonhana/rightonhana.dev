0e1011c4aec3bd66c52a0f5ac5b3221d3a420353
Florencia Gonzalez <git@rightonhana.dev>
2024-12-12 09:34:33 -0600

    :fire: remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
   integrations: [svelte()],
   output: 'server',
 });
